4eaf57ee6e334eb2dc246b5c01111735022ceded
gytks4 <gytks4@naver.com>
2021-10-04 21:23:34 +0900

    feat: meta tags 주석으로 임의 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,23 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <title></title>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          />
+          {/* <link rel="icon" href="/assets/icons/favicon.svg" />
+          <link rel="canonical" href="https://www.speechtime.co.kr" />
+          <meta name="description" content="" />
+          <meta name="keywords" content="" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="" />
+          <meta property="og:url" content="" />
+          <meta property="og:site_name" content="" />
+          <meta property="og:description" content="" />
+          <meta property="og:image" content="" /> */}
         </Head>
         <body>
           <div id="root">
