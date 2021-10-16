0fad3e20cceb033b32d144730afcd6c2e15276df
nahee-park <jessica396@ewhain.net>
2021-10-16 15:55:26 +0900

    dev: stortybook 절대 경로 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-styled-component-theme/dist/preset',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  webpackFinal: async (config) => {
+    // node_mules폴더와 styles 폴더 안의 모듈을 인식할 수 있게 함
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'styles'];
+
+    // 절대 경로 설정
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../components'),
+      '@assets': path.resolve(__dirname, '../public/assets'),
+    };
+
+    return config;
+  },
+};
