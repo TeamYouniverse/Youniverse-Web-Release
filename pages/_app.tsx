41740898c9cba38d51439a52b9480ea857ea61ee
nahee-park <jessica396@ewhain.net>
2022-03-08 17:26:38 +0900

    Revert "dev: redux 초기세팅"
    This reverts commit 9bc30efd50ed22b7c75980b219ded3c5e2092757.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
+    <RecoilRoot>
       <Component {...pageProps} />
-    </Provider>
+    </RecoilRoot>
   );
 }
 
--- redux/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import counter from './example/reducer';
-
-const rootReducer = combineReducers({
-  counter,
-});
-// 여러 reducer를 rootReducer에 합쳐서 store로 보낸다. store는 하나여야하기 떄문.
-
-export default rootReducer;
