67131279aac69ad0312a129696188d26413a5f7c
nahee-park <jessica396@ewhain.net>
2022-04-01 04:33:51 +0900

    dev: theme color 세팅, default font 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@src/styles/globals.css';
+// import Head from 'next/head';
 import { wrapper } from '@store';
+import { ThemeProvider } from '@emotion/react';
+import theme from '@style/theme';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 export default wrapper.withRedux(MyApp);
---
+++ src/lib/style/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  color: {
+    black: '#050416',
+    middleGray: '#504F5C',
+    lightGray: '#9D9D9D',
+    white: '#FFFFFF',
+  },
+};
+
+export default theme;
