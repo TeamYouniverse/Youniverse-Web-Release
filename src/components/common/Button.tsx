ee76c52a163403f368f721772da3c904364f0ded
Danna <daeunlee0720@gmail.com>
2022-04-01 18:42:50 +0900

    Auto stash before merge of "feat/button-component" and "develop"

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface iButtonProps {
+  title: String;
+  onClick: () => {};
+}
+
+function Button({ title, onClick }: iButtonProps) {
+  const data = {};
+  return <div>Button</div>;
+}
+
+export default Button;
+
+const Styled = {};
