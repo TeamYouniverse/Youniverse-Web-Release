00a144acb220723bb7646518f9447cdd13981dd5
Danna <daeunlee0720@gmail.com>
2022-04-01 19:49:18 +0900

    feat: 헤더 컴포넌트 제작

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import styled from '@emotion/styled';
+import React from 'react';
+import Image from 'next/image';
+import logoImage from '@assets/logo.svg';
 
 function Header() {
-  return <div>123</div>;
+  return (
+    <Styled.ImageWrapper>
+      <Image src={logoImage} alt="Youniverse Logo" />
+    </Styled.ImageWrapper>
+  );
 }
 
 export default Header;
+
+const Styled = {
+  ImageWrapper: styled.image`
+    padding-top: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+};
