f52d81876f973e06c728e4a4f7220e997c964799
Danna <daeunlee0720@gmail.com>
2022-04-01 19:10:17 +0900

    feat: 버튼 컴포넌트 제작

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@store/modules';
 import * as userActions from '@store/modules/user';
+import Button from '@src/components/common/Button';
 
 function index() {
   // 정의된 리듀서를 사용할 수 있도록 해주는 애 : dispatch
@@ -45,7 +46,7 @@ function index() {
         <h1>새로운 유저 아이디, 이름 입력 후 버튼을 눌러주세요</h1>
         <input id="id" placeholder="유저 아이디" onChange={onChangeHandler} />
         <input id="name" placeholder="유저 이름" onChange={onChangeHandler} />
-        <button onClick={onClickHandler}>반영하기</button>
+        <Button title="반영하기" onClick={onClickHandler} />
       </section>
     </>
   );
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,15 +1,23 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 interface iButtonProps {
   title: String;
-  onClick: () => {};
+  onClick: () => void;
 }
 
 function Button({ title, onClick }: iButtonProps) {
   const data = {};
-  return <div>Button</div>;
+  return <Styled.CommonButton onClick={onClick}>{title}</Styled.CommonButton>;
 }
 
 export default Button;
 
-const Styled = {};
+const Styled = {
+  CommonButton: styled.button`
+    color: white;
+    border-bottom: 1px solid white;
+    padding: 0px 11px 8px 11px;
+    font-family: 'JejuMyeongjo';
+  `,
+};
