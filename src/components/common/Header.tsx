b3764dd9d079e0e4863bd92e51249116b284dcff
Danna <daeunlee0720@gmail.com>
2022-04-01 20:24:13 +0900

    feat: 로고 클릭시 홈으로 라우팅

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,23 +1,37 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { useRouter } from 'next/router';
+import youniverseLogo from '@assets/logo.svg';
 import Image from 'next/image';
-import logoImage from '@assets/logo.svg';
 
 function Header() {
+  const router = useRouter();
+
+  const handleLogoClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    router.push('/');
+  };
+
   return (
-    <Styled.ImageWrapper>
-      <Image src={logoImage} alt="Youniverse Logo" />
-    </Styled.ImageWrapper>
+    <Styled.LogoWrapper>
+      <Styled.Logo onClick={handleLogoClick} />
+    </Styled.LogoWrapper>
   );
 }
 
 export default Header;
 
 const Styled = {
-  ImageWrapper: styled.image`
-    padding-top: 15px;
+  LogoWrapper: styled.header`
+    margin-top: 15px;
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 100%;
+  `,
+  Logo: styled.button`
+    background: url(${youniverseLogo.src}) no-repeat;
+    width: 150px;
+    height: 56px;
   `,
 };
